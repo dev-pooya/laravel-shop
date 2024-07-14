d31d4925a2be383eed7908d37ae58c442e903cfb
dev-pooya <pooya.sadeghi1997@gmail.com>
2024-07-14 13:23:15 +0330

    material UI and breeze added.

---
+++ resources/js/Pages/Test/Index.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+function Index() {
+    return (
+        <div className="p-8">
+            <h1>test for material ui</h1>
+
+            <Button variant="contained">Text</Button>
+        </div>
+    );
+}
+
+export default Index;
